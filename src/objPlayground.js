e3959f57eb0516c91acdc1555d01acec147ffc39
Gabriel Freitas <gabrielkelvinfreitas@gmail.com>
2022-11-25 10:54:55 -0300

    REQ7 - Finalizado, função para calcular e gerar array feita.

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -34,8 +34,28 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 0, sub: -1 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 0 ], [ 'sub', -1 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  const multiCalc = {
+    sum: Math.floor(number1 + number2),
+    mult: Math.floor(number1 * number2),
+    div: Math.floor(number1 / number2),
+    sub: Math.floor(number1 - number2),
+  };
+  return multiCalc;
+};
+console.log(calculator(1, 2));
+const arrayGenerator = (type, object) => {
+  if (type === 'keys') {
+    return Object.keys(object);
+  }
+  if (type === 'values') {
+    return Object.values(object);
+  }
+  if (type === 'entries') {
+    return Object.entries(object);
+  }
+};
 
-const arrayGenerator = (type, object) => {};
+console.log(arrayGenerator('values', calculator(1, 2)));
 
 module.exports = { calculator, arrayGenerator };
